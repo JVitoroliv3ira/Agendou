28849b31d39b7a6b352a8feae1d0963f4b986a10
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-20 19:14:51 -0300

    feat(#1): Adiciona service para a entidade User

---
+++ api/src/main/java/api/services/UserService.java
@@ -0,0 +1,17 @@
+package api.services;
+
+import api.domain.models.User;
+import api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User create(User user) {
+        user.setId(null);
+        return this.userRepository.save(user);
+    }
+}
