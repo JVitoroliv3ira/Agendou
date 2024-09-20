86851cf997fabf1993b667432f8ea90c00d5bfd3
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-20 20:06:12 -0300

    feat(#1): Adiciona método para validar a unicidade do email durante o cadastro

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,8 +1,10 @@
 package api.services;
 
 import api.domain.models.User;
+import api.exceptions.ApiException;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,8 +12,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    private static final String EMAIL_NOT_UNIQUE_ERROR = "Este e-mail já está associado a uma conta existente.";
+
     public User create(User user) {
         user.setId(null);
         return this.userRepository.save(user);
+    }
+
+    public void validateEmailUniqueness(String email) {
+        if (this.userRepository.existsByEmail(email)) {
+            throw new ApiException(
+                    EMAIL_NOT_UNIQUE_ERROR,
+                    "email",
+                    HttpStatus.BAD_REQUEST
+            );
+        }
     }
 }
