43936a2242251040636b5ad04fe2da575e25eff3
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-21 17:54:45 -0300

    feat(#1): Substitui os inputs padrão pelo componente form-input

--- web/src/app/modules/auth/auth.module.ts
+++ web/src/app/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RegisterPageComponent} from './pages/register-page/register-page.component';
 import {AuthRoutingModule} from "./auth-routing.module";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import {AuthRoutingModule} from "./auth-routing.module";
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule {
--- web/src/app/modules/auth/pages/register-page/register-page.component.ts
+++ web/src/app/modules/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'agendou-register-page',
   templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
-
+  public handleInputChangeEvent(input: string, value: string): void {
+    console.log(`${input}: ${value}`);
+  }
 }
