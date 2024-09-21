a8e9ae9d38a864e0557b3a0998993bc877560234
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-20 22:05:20 -0300

    feat(#1): Adiciona metodo loadUserById

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.domain.dtos.Details;
 import api.domain.models.User;
 import api.exceptions.ApiException;
 import api.repositories.UserRepository;
@@ -13,6 +14,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     private static final String EMAIL_NOT_UNIQUE_ERROR = "Este e-mail já está associado a uma conta existente.";
+    private static final String USER_NOT_FOUND_ERROR = "Usuário não encontrado na base de dados.";
 
     public User create(User user) {
         user.setId(null);
@@ -27,5 +29,17 @@ public class UserService {
                     HttpStatus.BAD_REQUEST
             );
         }
+    }
+
+    public Details loadUserById(Long id) throws ApiException {
+        User user = this.userRepository
+                .findById(id)
+                .orElseThrow(() -> new ApiException(
+                        USER_NOT_FOUND_ERROR,
+                        "id",
+                        HttpStatus.BAD_REQUEST
+                ));
+
+        return new Details(user);
     }
 }
