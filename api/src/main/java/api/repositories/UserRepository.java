4dfbfefd28db82c4005ce77f989a0cdc58e57108
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-20 21:31:57 -0300

    feat(#1): Adiciona método responsável por buscar usuario pelo email

--- api/src/main/java/api/repositories/UserRepository.java
+++ api/src/main/java/api/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import api.domain.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByEmail(String email);
+    Boolean existsByEmailIgnoreCase(String email);
+    Optional<User> findByEmailIgnoreCase(String email);
 }
