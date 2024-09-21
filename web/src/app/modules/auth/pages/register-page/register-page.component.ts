fb406af3a6e19b153009d2b0db9f5774ce255c90
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-20 23:03:41 -0300

    feat(#1): Adiciona componente para a pagina de registro

---
+++ web/src/app/modules/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'agendou-register-page',
+  templateUrl: './register-page.component.html'
+})
+export class RegisterPageComponent {
+
+}
