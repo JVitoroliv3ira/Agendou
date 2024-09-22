0746266f50517fbea1ca3abcfde44d9f4d2eede2
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-21 23:48:33 -0300

    feat(#1): Adiciona guard para garantir que o usuario esteja autenticado

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from "./core/guards/auth-guard";
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'app',
-    loadChildren: () => import('./modules/app/app.module').then(m => m.AppModule)
+    loadChildren: () => import('./modules/app/app.module').then(m => m.AppModule),
+    canActivate: [AuthGuard],
   }
 ];
 
