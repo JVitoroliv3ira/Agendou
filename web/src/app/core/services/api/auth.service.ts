a421d520dcb292b1c95261b9e0d0cbbfcf67839e
João Oliveira <jvitoroliveira0711@gmail.com>
2024-09-21 23:22:49 -0300

    feat(#1): Adiciona metodo responsável por autenticar o usuario

--- web/src/app/core/services/api/auth.service.ts
+++ web/src/app/core/services/api/auth.service.ts
@@ -4,6 +4,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ApiResponse} from "../../domain/dtos/responses/api-response";
 import {UserRegistrationRequest} from "../../domain/dtos/requests/user-registration-request";
+import {UserAuthenticationRequest} from "../../domain/dtos/requests/user-authentication-request";
+import {AuthenticatedUserResponse} from "../../domain/dtos/responses/authenticated-user-response";
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +18,13 @@ export class AuthService extends AbstractHttpService {
   public register(request: UserRegistrationRequest): Observable<ApiResponse<string>> {
     return this.post<ApiResponse<string>>(
       "http://localhost:8080/api/v1/auth/register",
+      request
+    );
+  }
+
+  public login(request: UserAuthenticationRequest): Observable<ApiResponse<AuthenticatedUserResponse>> {
+    return this.post<ApiResponse<AuthenticatedUserResponse>>(
+      "http://localhost:8080/api/v1/auth/token",
       request
     );
   }
